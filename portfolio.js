a0eee10a34edc3e389e00a89edcd402519c4bb17
Zuzeth Romo <zuzethromo@yahoo.com>
2019-09-12 14:35:53 -0600

    added files

---
+++ portfolio.js
@@ -0,0 +1,16 @@
+let modal = document.getElementById("myModal");
+
+var img = document.getElementById("resume");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() { 
+  modal.style.display = "none";
+}
